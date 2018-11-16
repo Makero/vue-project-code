390d619acfb03621f20246e699fc432d169be0cb
Maker <lcwjyouyi@163.com>
2018-11-16 15:56:01 +0800

    提交项目框架

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <ol>
+      <li>商品</li>
+      <li>评论</li>
+      <li>商家</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.header
+  ol
+    display:flex; width:100%; height:40px; line-height:40px;
+    li
+      flex:1; text-align:center;
+</style>
